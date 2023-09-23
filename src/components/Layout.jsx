aa091a3b7a7e9004a1eb353dd4db1d95e65018a3
Ameyalli <ameyanezcmm@gmail.com>
2023-09-22 20:00:58 -0600

    Creación layout principal init

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,12 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
     return(
         <div className="md:flex md:min-h-screen">
-            <div className="md:w-1/4 bg-blue-900 px-5 py-10"></div>
+            <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+                <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+            </aside>
 
             <div className="md:w-3/4 p-10 md:h-screen overflow-scroll"></div>
+            <Outlet/>
         </div>
     )
 }
